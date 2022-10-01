44e190322063cb2ff40a52485e2b967ef70ea5e3
Jan Jürgen Luzgin <jaluzg@ttu.ee>
2022-10-01 23:10:13 +0300

    fully realiseeritud projekt :))

--- clientapp/src/components/StudentsList.vue
+++ clientapp/src/components/StudentsList.vue
@@ -1,21 +1,19 @@
 <template>
-  <div
-    class="min-h-screen flex justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 text-dark-300"
-  >
+  <div class="bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 text-dark-300">
     <div class="text-center bg-gray-50">
       <h1 class="font-bold">{{ title }}</h1>
-      <ul>
-        <li v-for="item in students" :key="item.studentCode">
-          <Student :student="item" />
-        </li>
-      </ul>
+      <DataTable :value="students">
+        <Column field="studentCode" header="Student Code" />
+        <Column field="firstName" header="First Name" />
+        <Column field="lastName" header="Student Code" />
+        <Column field="status" header="Status" />
+      </DataTable>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useStudentStore } from "@/stores/studentStore";
-import Student from "./Student.vue";
 
 defineProps<{ title: String }>();
 const { students } = useStudentStore();
--- clientapp/src/main.ts
+++ clientapp/src/main.ts
@@ -7,10 +7,15 @@ import PrimeVue from "primevue/config";
 import "primevue/resources/themes/saga-blue/theme.css"; //theme
 import "primevue/resources/primevue.min.css"; //core css
 import "primeicons/primeicons.css"; //icons
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
 import router from "@/router";
 
 const app = createApp(App);
 app.use(createPinia());
 app.use(PrimeVue);
 app.use(router);
+
+app.component("DataTable", DataTable);
+app.component("Column", Column);
 app.mount("#app");
